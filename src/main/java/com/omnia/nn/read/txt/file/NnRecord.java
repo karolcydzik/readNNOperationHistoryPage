2db2bf8c7c03178b398cc400cfd8a86e8a9f1bc7
karolcydzik <karol.cydzik@gmail.com>
2022-01-25 21:28:28 +0100

    first working copy

---
+++ src/main/java/com/omnia/nn/read/txt/file/NnRecord.java
@@ -0,0 +1,67 @@
+package com.omnia.nn.read.txt.file;
+
+public class NnRecord {
+    String dataZlecenia;
+    String dataWyceny;
+    String nazwaFunduszu;
+    String typTransakcji;
+    String status;
+    String kwota;
+    String wartoscRejestru;
+
+    public String getDataZlecenia() {
+        return dataZlecenia;
+    }
+
+    public void setDataZlecenia(String dataZlecenia) {
+        this.dataZlecenia = dataZlecenia;
+    }
+
+    public String getDataWyceny() {
+        return dataWyceny;
+    }
+
+    public void setDataWyceny(String dataWyceny) {
+        this.dataWyceny = dataWyceny;
+    }
+
+    public String getNazwaFunduszu() {
+        return nazwaFunduszu;
+    }
+
+    public void setNazwaFunduszu(String nazwaFunduszu) {
+        this.nazwaFunduszu = nazwaFunduszu;
+    }
+
+    public String getTypTransakcji() {
+        return typTransakcji;
+    }
+
+    public void setTypTransakcji(String typTransakcji) {
+        this.typTransakcji = typTransakcji;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getKwota() {
+        return kwota;
+    }
+
+    public void setKwota(String kwota) {
+        this.kwota = kwota;
+    }
+
+    public String getWartoscRejestru() {
+        return wartoscRejestru;
+    }
+
+    public void setWartoscRejestru(String wartoscRejestru) {
+        this.wartoscRejestru = wartoscRejestru;
+    }
+}
---
+++ src/main/java/com/omnia/nn/read/txt/file/ReadFileLineByLineUsingBufferedReader.java
@@ -0,0 +1,122 @@
+package com.omnia.nn.read.txt.file;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class ReadFileLineByLineUsingBufferedReader {
+
+    private static final String FILE_HEADER ="Data zlecenia\tData wyceny\tNazwa funduszu\tTyp transakcji\tStatus\tKwota\tWartosc po transakcji\n";
+    private static final String TAB_DELIMITER = "\t";
+    private static final String NEW_LINE_SEPARATOR = "\n";
+
+    private List<NnRecord> transactions = new ArrayList<>();
+    private String fileNameA = "/home/karolcydzik/IdeaProjects/readNNOperationHistoryPage/src/main/resources/NNTFI24A.html";
+    private String fileNameB = "/home/karolcydzik/IdeaProjects/readNNOperationHistoryPage/src/main/resources/NNTFI24B.html";
+    private String outputFileName = "outputFile.tsv";
+
+    public static void main(String[] args) {
+        ReadFileLineByLineUsingBufferedReader reader = new ReadFileLineByLineUsingBufferedReader();
+        reader.redFil(reader.fileNameA);
+        reader.redFil(reader.fileNameB);
+        reader.writeToTsv(reader.outputFileName);
+    }
+
+    private void writeToTsv(String outputFileName) {
+        try {
+            FileWriter fw = new FileWriter(outputFileName);
+            fw.append(FILE_HEADER.toString());
+            fw.append(NEW_LINE_SEPARATOR);
+            Iterator<NnRecord> iter = transactions.iterator();
+            while (iter.hasNext()) {
+                NnRecord nnRecord = iter.next();
+                StringBuilder sb = new StringBuilder();
+                sb.append(nnRecord.getDataZlecenia());
+                sb.append(TAB_DELIMITER);
+                sb.append(nnRecord.getDataWyceny());
+                sb.append(TAB_DELIMITER);
+                sb.append(nnRecord.getNazwaFunduszu());
+                sb.append(TAB_DELIMITER);
+                sb.append(nnRecord.getTypTransakcji());
+                sb.append(TAB_DELIMITER);
+                sb.append(nnRecord.getStatus());
+                sb.append(TAB_DELIMITER);
+                sb.append(nnRecord.getKwota());
+                sb.append(TAB_DELIMITER);
+                sb.append(nnRecord.getWartoscRejestru());
+                sb.append(NEW_LINE_SEPARATOR);
+                try {
+                    fw.append(sb.toString());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    iter.remove();
+                }
+            }
+            fw.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void redFil(String fileName) {
+        BufferedReader reader;
+        Pattern datePattern = Pattern.compile("\\d+");
+        boolean started = false;
+        NnRecord nnRecord = new NnRecord();
+        try {
+            reader = new BufferedReader(new FileReader(fileName));
+            String line = reader.readLine();
+            while (line != null && line.indexOf("<nav aria-label=\"...\" class=\"text-right\">") < 0) {
+                if (!started) {
+                    if (line.indexOf("rejestru<br>po transakcji") > 0) {
+                        started = true;
+                    }
+                } else {
+                    if (line.indexOf("break-date") > 0) {
+                        Matcher dateMatcher = datePattern.matcher(line);
+                        List<String> dtLs = new ArrayList(3);
+                        while (dateMatcher.find()) {
+                            dtLs.add(dateMatcher.group());
+                        }
+                        String stDate = dtLs.get(2) + "-" + dtLs.get(1) + "-" + dtLs.get(0);
+                        if (nnRecord.getDataZlecenia() == null) {
+                            nnRecord.setDataZlecenia(stDate);
+                        } else {
+                            nnRecord.setDataWyceny(stDate);
+                        }
+                    } else if (line.indexOf("<p class=\"name\">") > 0) {
+                        String name = StringUtils.substringBetween(line, ">", "<");
+                        nnRecord.setNazwaFunduszu(name);
+                    } else if (line.indexOf("margin-xs text-center text-left-xs") > 0) {
+                        String typTransakcji = StringUtils.substringBetween(line, ">", "<");
+                        nnRecord.setTypTransakcji(typTransakcji);
+                    } else if (line.indexOf("<p class=\"text-center\">") > 0) {
+                        String status = StringUtils.substringBetween(line, ">", "<");
+                        nnRecord.setStatus(status);
+                    } else if (line.indexOf("value margin-xs text-center text-right-xs") > 0) {
+                        String kwota = StringUtils.substringBetween(line, "</span>", "</p>");
+                        nnRecord.setKwota(kwota);
+                    } else if (line.indexOf("registry-value") > 0) {
+                        String kwota = StringUtils.substringBetween(line, ">", "<");
+                        nnRecord.setWartoscRejestru(kwota);
+                        transactions.add(nnRecord);
+                        nnRecord = new NnRecord();
+                    }
+                }
+                line = reader.readLine();
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
